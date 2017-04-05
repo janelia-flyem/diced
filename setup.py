2c7af990855a80f57591fca1b77ac02c914194b7
Stuart Berg <bergs@janelia.hhmi.org>
2017-04-05 12:04:40 -0400

    conda-recipe enhancements: - Use 'path:' instead of 'git_url:' - Version is auto-chosen from the git tag - run_test.sh works on installed package (doesn't rely on setup.py)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-packages=['diced'
-         ]
+packages=['diced',
+          'diced.tests']
 
 setup(name='diced',
       version='0.2',
@@ -9,5 +9,6 @@ setup(name='diced',
       url='https://github.com/janelia-flyem/DVIDCloudStore',
       packages=packages,
       include_package_data=True,
-      test_suite="diced.tests"
+      test_suite="diced.tests",
+      zip_safe=False # So we can run the tests after installation
       )
